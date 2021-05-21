cb04f1de2ead7c640a0571808962f62765c805d0
tobeqz <83136264+tobeqz@users.noreply.github.com>
2021-05-21 16:18:18 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -283,7 +283,7 @@ class MicroPong {
     constructor() {
         this.p1 = new Player(0, 0)        
         this.p2 = new Player(9, 0)
-        this.ball = new Ball(5, 0, 1)
+        this.ball = new Ball(1, 0, 1)
         this.ownPlayer = handshake.isServer ? this.p1 : this.p2
 
         r_events.registerEvent("render")
@@ -308,14 +308,15 @@ class MicroPong {
                 // Player 2's x coord is altijd 9
                 this.p2.y = parseInt(pos_as_str)
             })
-        } 
 
-        control.inBackground(() => {
-            while (true) {
-                control.wait(this.ballInterval*1000000)
-                this.ball.moveX(this.ball.velocity)
-            }
-        })
+            control.inBackground(() => {
+                while (true) {
+                    basic.pause(this.ballInterval * 1000)
+                    this.ball.moveX(this.ball.velocity)
+                    console.log(this.ball.x)
+                }
+            })
+        } 
     }
 
     render() {
