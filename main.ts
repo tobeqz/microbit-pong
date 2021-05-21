d458d2ff332d81b7ca13379c00ab67b2ddde8ecd
tobeqz <83136264+tobeqz@users.noreply.github.com>
2021-05-21 12:49:56 +0200

    add radioclass

--- main.ts
+++ main.ts
@@ -1,3 +1,50 @@
+/* 
+ * Deze class is nodig omdat je maximaal 18 bytes
+ * aan data kunt sturing via 1 sendString() call
+ * dit staat HELEMAAL NERGENS in de documentatie
+ * en ik kwam er na iets van 3 uur debuggen achter
+ * dus bij deze, een radio wrapper die makecode eigenlijk
+ * zelf moet supplyen als ze "beginnner friendly" willen
+ * zijn. Het support alleen strings, omdat JSON.parse en
+ * JSON.stringify te veel moeite waren voor microsoft
+ * om te implementeren.
+ */
+class RadioWrapper {
+    callbacks: Function[]
+    
+    constructor(radioGroup: number) {
+        radio.setGroup(radioGroup)
+        this.callbacks = []
+
+        let full_string = ""
+        radio.onReceivedString(slice => {
+            full_string += slice
+            if (slice[slice.length-1] == "\u{03}") {
+                for (const callback of this.callbacks) {
+                    callback(full_string)
+                }
+
+                full_string = ""
+            }
+        })
+    }
+
+    sendString(stringToSend: string) {
+        const string_with_boundary = `\u{02}${stringToSend}\u{03}`
+        // 02 en 03 staan in ASCII voor start en einde respectievelijk
+
+        for (let i = 0; i < string_with_boundary.length; i += 18) {
+            const slice = string_with_boundary.substr(i, 18)
+            radio.sendString(slice)
+        }
+    }
+    
+    onReceive(callback: Function) {
+        this.callbacks.push(callback)        
+    }
+}
+
+
 /**
  * Bij lager
  */
